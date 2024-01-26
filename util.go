b04010f3424254a7322d5f82bb3750a6f33a7c84
Kenneth J. Pronovici <pronovic@ieee.org>
2024-01-26 16:12:11 -0600

    Initial revision

---
+++ util.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"errors"
+	"github.com/golang-ds/queue/circularqueue"
+	"github.com/google/go-cmp/cmp"
+)
+
+// CircularQueue is a fixed-sized queue that keeps returning its contents repeatedly, in order.
+type CircularQueue[T any] struct {
+	wrapped *queue.CircularQueue[T]
+}
+
+// New constructs a circular queue with the contents of an array or slice.
+func New[T any](entries []T) CircularQueue[T] {
+	wrapped := queue.New[T]()
+	q := CircularQueue[T]{&wrapped}
+
+	for _, e := range entries {
+		q.wrapped.Enqueue(e)
+	}
+
+	return q
+}
+
+// SetFirst moves the indicated entry to the front of the circular queue, returning an error if not found
+func (q *CircularQueue[T]) SetFirst(entry T) error {
+	if q.wrapped.IsEmpty() {
+		return errors.New("queue is empty")
+	}
+
+	for i := 0; i < q.wrapped.Size(); i++ {
+		var first, _ = q.wrapped.First()
+
+		if diff := cmp.Diff(entry, first); diff == "" {
+			return nil // entry is found at front of queue
+		}
+
+		q.wrapped.Rotate() // rotate to the next entry and try again
+	}
+
+	return errors.New("entry not found")
+}
+
+// Next gets the next entry in the queue, wrapping around to the front if needed
+func (q *CircularQueue[T]) Next() (T, error) {
+	if q.wrapped.IsEmpty() {
+		return *new(T), errors.New("queue is empty")
+	}
+
+	var first, ok = q.wrapped.First()
+	if !ok {
+		return *new(T), errors.New("entry not found")
+	}
+
+	q.wrapped.Rotate()
+
+	return first, nil
+}
---
+++ util_test.go
@@ -0,0 +1,119 @@
+package main
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestEmpty(t *testing.T) {
+	var err error
+	var entries []string
+
+	queue := New[string](entries)
+
+	err = queue.SetFirst("")
+	assert.EqualError(t, err, "queue is empty")
+
+	err = queue.SetFirst("a")
+	assert.EqualError(t, err, "queue is empty")
+
+	_, err = queue.Next()
+	assert.EqualError(t, err, "queue is empty")
+}
+
+func TestSingle(t *testing.T) {
+	var entry string
+	var err error
+	var entries = []string{"a"}
+
+	queue := New[string](entries)
+
+	for j := 0; j <= 10; j++ {
+		entry, err = queue.Next()
+		assert.Nil(t, err)
+		assert.Equal(t, "a", entry)
+	}
+
+	err = queue.SetFirst("")
+	assert.EqualError(t, err, "entry not found")
+
+	err = queue.SetFirst("a")
+	assert.Nil(t, err)
+	entry, err = queue.Next()
+	assert.Nil(t, err)
+	assert.Equal(t, "a", entry)
+
+	err = queue.SetFirst("d")
+	assert.EqualError(t, err, "entry not found")
+
+	err = queue.SetFirst("f")
+	assert.EqualError(t, err, "entry not found")
+}
+
+func TestMultiple(t *testing.T) {
+	var entry string
+	var err error
+	var entries = []string{"a", "b", "c", "d", "e"}
+
+	queue := New[string](entries)
+
+	entry, err = queue.Next()
+	assert.Nil(t, err)
+	assert.Equal(t, "a", entry)
+
+	entry, err = queue.Next()
+	assert.Nil(t, err)
+	assert.Equal(t, "b", entry)
+
+	entry, err = queue.Next()
+	assert.Nil(t, err)
+	assert.Equal(t, "c", entry)
+
+	entry, err = queue.Next()
+	assert.Nil(t, err)
+	assert.Equal(t, "d", entry)
+
+	entry, err = queue.Next()
+	assert.Nil(t, err)
+	assert.Equal(t, "e", entry)
+
+	entry, err = queue.Next()
+	assert.Nil(t, err)
+	assert.Equal(t, "a", entry)
+
+	entry, err = queue.Next()
+	assert.Nil(t, err)
+	assert.Equal(t, "b", entry)
+
+	err = queue.SetFirst("")
+	assert.EqualError(t, err, "entry not found")
+
+	entry, err = queue.Next()
+	assert.Nil(t, err)
+	assert.Equal(t, "c", entry) // this is the same place we would otherwise have been
+
+	err = queue.SetFirst("a")
+	assert.Nil(t, err)
+	entry, err = queue.Next()
+	assert.Nil(t, err)
+	assert.Equal(t, "a", entry)
+	entry, err = queue.Next()
+	assert.Nil(t, err)
+	assert.Equal(t, "b", entry)
+
+	err = queue.SetFirst("d")
+	assert.Nil(t, err)
+	entry, err = queue.Next()
+	assert.Nil(t, err)
+	assert.Equal(t, "d", entry)
+	entry, err = queue.Next()
+	assert.Nil(t, err)
+	assert.Equal(t, "e", entry)
+
+	err = queue.SetFirst("f")
+	assert.EqualError(t, err, "entry not found")
+
+	entry, err = queue.Next()
+	assert.Nil(t, err)
+	assert.Equal(t, "a", entry) // this is the same place we would otherwise have been
+}
