2538152078697a5f80d7a511415bdce8dbfc57df
Kenneth J. Pronovici <pronovic@ieee.org>
2024-02-09 16:39:49 -0600

    Stub out test cases

---
+++ engine/engine_test.go
@@ -0,0 +1,101 @@
+package engine
+
+import (
+	"testing"
+)
+
+func TestNewEngine(t *testing.T) {
+	t.Fail()  // TODO: implement test case
+}
+
+func TestEngineStarted(t *testing.T) {
+	t.Fail()  // TODO: implement test case
+}
+
+func TestEngineCompleted(t *testing.T) {
+	t.Fail()  // TODO: implement test case
+}
+
+func TestEngineWinner(t *testing.T) {
+	t.Fail()  // TODO: implement test case
+}
+
+func TestEngineReset(t *testing.T) {
+	t.Fail()  // TODO: implement test case
+}
+
+func TestEngineStartGame(t *testing.T) {
+	t.Fail()  // TODO: implement test case
+}
+
+func TestEngineDraw(t *testing.T) {
+	t.Fail()  // TODO: implement test case
+}
+
+func TestEngineDiscard(t *testing.T) {
+	t.Fail()  // TODO: implement test case
+}
+
+func TestEngineConstructLegalMovesStandardNoCard(t *testing.T) {
+	t.Fail()  // TODO: implement test case
+}
+
+func TestEngineConstructLegalMovesStandardCard(t *testing.T) {
+	t.Fail()  // TODO: implement test case
+}
+
+func TestEngineConstructLegalMovesAdultNoCard(t *testing.T) {
+	t.Fail()  // TODO: implement test case
+}
+
+func TestEngineConstructLegalMovesAdultCard(t *testing.T) {
+	t.Fail()  // TODO: implement test case
+}
+
+func TestEnginePlayNextCompleted(t *testing.T) {
+	t.Fail()  // TODO: implement test case
+}
+
+func TestEnginePlayNextFailed(t *testing.T) {
+	t.Fail()  // TODO: implement test case
+}
+
+func TestEnginePlayNextStandardForfeit(t *testing.T) {
+	t.Fail()  // TODO: implement test case
+}
+
+func TestEnginePlayNextStandardIllegal(t *testing.T) {
+	t.Fail()  // TODO: implement test case
+}
+
+func TestEnginePlayNextStandardLegal(t *testing.T) {
+	t.Fail()  // TODO: implement test case
+}
+
+func TestEnginePlayNextStandardDrawAgain(t *testing.T) {
+	t.Fail()  // TODO: implement test case
+}
+
+func TestEnginePlayNextStandardComplete(t *testing.T) {
+	t.Fail()  // TODO: implement test case
+}
+
+func TestEnginePlayNextAdultForfeit(t *testing.T) {
+	t.Fail()  // TODO: implement test case
+}
+
+func TestEnginePlayNextAdultIllegal(t *testing.T) {
+	t.Fail()  // TODO: implement test case
+}
+
+func TestEnginePlayNextAdultLegal(t *testing.T) {
+	t.Fail()  // TODO: implement test case
+}
+
+func TestEnginePlayNextAdultDrawAgain(t *testing.T) {
+	t.Fail()  // TODO: implement test case
+}
+
+func TestEnginePlayNextAdultComplete(t *testing.T) {
+	t.Fail()  // TODO: implement test case
+}
